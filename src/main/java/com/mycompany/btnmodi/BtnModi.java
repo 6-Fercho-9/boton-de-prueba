821266a5a33dc67e891708a01172b55fde2b5e3f
Fernando <>
2024-06-01 16:02:16 -0400

    test

---
+++ src/main/java/com/mycompany/btnmodi/BtnModi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.btnmodi;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class BtnModi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
